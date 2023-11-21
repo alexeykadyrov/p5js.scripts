475137a65ec297d80c70ac6ec994b9e9cb3e8d3b
Alexey Kadyrov <70022815+alexeykadyrov@users.noreply.github.com>
2023-11-21 17:38:13 +0300

    add bayer_dithering.js

---
+++ bayer_dithering.js
@@ -0,0 +1,63 @@
+// Этот скрипт реализует технику «Дизеринга Байера», используя матрицу сглаживания размером 2x2
+// https://surma.dev/things/ditherpunk/
+// https://habr.com/ru/companies/wunderfund/articles/680154/
+
+let img;
+let imgWidth, imgHeight;
+let M_dith;
+let M_dith_d = 2;
+
+function preload() {
+    img = loadImage('IMG/*.jpeg'); // Путь к файлу с картинкой
+}
+
+function setup() {
+    createCanvas(img.width, img.height);
+    img.loadPixels();
+    imgWidth = img.width;
+    imgHeight = img.height;
+
+    // Матрица сглаживания Байера 2×2
+    M_dith = [
+        [2**.5, 3.1415], // Первичные значения [1, 3],
+        [4, 1.618]       // Первичные значения [4, 2]
+    ];
+
+    // Вызов функции для обработки изображения.
+    applyDithering();
+
+    // Отображение изображения после применения дизеринга.
+    image(img, 0, 0);
+    noLoop();
+}
+
+function applyDithering() {
+    for (let i = 0; i < img.pixels.length; i += 4) {
+        let x = (i / 4) % imgWidth;
+        let y = Math.floor((i / 4) / imgWidth);
+
+        // Получаем среднее значение RGB для вычисления яркости.
+        let pixel = (img.pixels[i] * 30 + img.pixels[i+1] * 59 + img.pixels[i+2] * 11) / 100;
+
+        // Вычисляем координаты в матрице сглаживания.
+        let x_dith = x % M_dith_d; // x-координата в матрице сглаживания.
+        let y_dith = y % M_dith_d; // y-координата в матрице сглаживания.
+
+        // Получаем пороговое значение из матрицы сглаживания.
+        let threshold = (M_dith[x_dith][y_dith] / 5) * 255;
+
+        // Сравниваем интенсивность текущего пикселя с пороговым значением матрицы сглаживания.
+        if (pixel > threshold) {
+            // Если интенсивность больше порога, устанавливаем цвет пикселя в белый.
+            img.pixels[i] = 255;
+            img.pixels[i + 1] = 255;
+            img.pixels[i + 2] = 255;
+        } else {
+            // Иначе устанавливаем цвет пикселя в черный.
+            img.pixels[i] = 0;
+            img.pixels[i + 1] = 0;
+            img.pixels[i + 2] = 0;
+        }
+    }
+    img.updatePixels();
+}
